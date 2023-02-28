4c2c3daee42cda8e61b16462c26bf8ed56ac419a
João Souza <joaosouz8@gmail.com>
2023-02-28 20:08:42 -0300

    correção

--- routes.js
+++ routes.js
@@ -1,6 +1,9 @@
 const routes = require('express').Router();
 const MusicController = require('./controller/musicController');
 
+routes.get('/', (req, res)=>{
+    res.status(200).json({message: "Tudo certo"})
+})
 routes.post('/register', MusicController.store);
 routes.get('/show', MusicController.show);
 
